bc1fdb80cda5063688e02f95bdde03168c557e65
MoveidNaqvi <moviednaqvi@gmail.com>
2022-06-16 12:44:44 +0100

    improved youtuber iframe player and removed unused imports

--- src/components/videoembed/YoutubeEmbed.jsx
+++ src/components/videoembed/YoutubeEmbed.jsx
@@ -8,7 +8,6 @@ const YoutubeEmbed = ({ embedId }) => (
       src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
       title="Embedded youtube"
     />
   </div>
