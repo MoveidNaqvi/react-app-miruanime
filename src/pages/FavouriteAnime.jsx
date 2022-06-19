055a0e5a62a68ba467ed7123b06f146947f831fd
MoveidNaqvi <moviednaqvi@gmail.com>
2022-06-19 15:42:20 +0100

    minor bug fixes

--- src/pages/FavouriteAnime.jsx
+++ src/pages/FavouriteAnime.jsx
@@ -11,7 +11,7 @@ function FavouriteAnime() {
   return (
     <div className='fav-anime'>
       {favouriteAnimeArray.data.length > 0 ? <h1>Your favourite anime!</h1> : ''}
-      {favouriteAnimeArray.data.length > 0 ? <AnimeList animelist={favouriteAnimeArray} type='favourite'/> : <h1>No favourite anime</h1>}
+      {favouriteAnimeArray.data.length > 0 ? <AnimeList animelist={favouriteAnimeArray} type='favourite'/> : <h1>No favourite anime!</h1>}
     </div>
   )
 }
