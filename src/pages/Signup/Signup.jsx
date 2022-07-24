c1060c5b9f12d3d0487a05b2318f028f95c1487f
MoveidNaqvi <moviednaqvi@gmail.com>
2022-07-24 21:54:43 +0100

    Signup logic along with form styling

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,5 +1,8 @@
-import { Link } from 'react-router-dom'
+import {createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import {auth} from '../../firebase/config'
+import { Link, useNavigate } from 'react-router-dom'
 import {useState} from 'react'
+import { toast } from 'react-toastify'
 import './Signup.css'
 
 function Signup() {
@@ -7,13 +10,34 @@ function Signup() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [loading, setIsLoading] = useState(false)
 
-  const handleRegister = (e) => {
+  const navigate = useNavigate()
+
+  const handleRegister = async (e) => {
     e.preventDefault()
-    console.log(name, email, password)
-    setName('')
-    setEmail('')
-    setPassword('')
+    try {
+      setIsLoading(true)
+      const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+      await updateProfile(auth.currentUser, {
+        displayName: name
+      })
+      setIsLoading(false)
+      setName('')
+      setEmail('')
+      setPassword('')
+      toast.success('Registration successful', {
+        autoClose: 1500,
+        pauseOnHover: false,
+      })
+      navigate('/favourite')
+    } catch (error) {
+      toast.error(error.message, {
+        autoClose: 1500,
+        pauseOnHover: false,
+      })
+      setIsLoading(false)
+    }
   }
 
   return (
@@ -21,18 +45,15 @@ function Signup() {
       <div className="sign-up">
         <form className="sign-up-form" onSubmit={handleRegister}>
           <label>
-            <span>Name</span>
-            <input type="text" onChange={(e) => setName(e.target.value)} value={name}/>
+            <input type="text" placeholder='Name' onChange={(e) => setName(e.target.value)} value={name} required/>
           </label>
           <label>
-            <span>Email</span>
-            <input type="email" onChange={(e) => setEmail(e.target.value)} value={email}/>
+            <input type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)} value={email} required/>
           </label>
           <label>
-            <span>Password</span>
-            <input type="password" onChange={(e) => setPassword(e.target.value)} value={password}/>
+            <input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} value={password} required/>
           </label>
-          <button className="sign-up-btn">Register</button>
+          <button className="sign-up-btn" disabled={loading}>{!loading ? 'Register' : 'loading...'}</button>
           <p className="alternative">Already have an account? <Link to='/sign-in' className='login-link'>Login here!</Link></p>
         </form>
       </div>
