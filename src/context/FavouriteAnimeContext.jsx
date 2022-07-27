66e616922e693a28cecde29cea55cd72a1715a80
MoveidNaqvi <moviednaqvi@gmail.com>
2022-07-27 12:16:58 +0100

    anime now added to firestore instead of local storage. Addition of uid property onto anime object.

--- src/context/FavouriteAnimeContext.jsx
+++ src/context/FavouriteAnimeContext.jsx
@@ -1,4 +1,6 @@
-import { createContext, useReducer, useEffect} from "react";
+import { createContext, useReducer} from "react";
+import { auth, db } from "../firebase/config";
+import {addDoc, deleteDoc, doc, collection} from 'firebase/firestore'
 import favouriteAnimeReducer from "./FavouriteAnimeReducer";
 
 const FavouriteAnimeContext = createContext()
@@ -6,21 +8,25 @@ const FavouriteAnimeContext = createContext()
 export const FavouriteAnimeProvider = ({children}) => {
 
   const initialState = {
-    favourites: localStorage.getItem('favourites') ? JSON.parse(localStorage.getItem('favourites')) : []
+    favourites: null
   }
 
   const [state, dispatch] = useReducer(favouriteAnimeReducer, initialState)
 
-  useEffect(() => {
-    localStorage.setItem('favourites', JSON.stringify(state.favourites))
-  }, [state])
-
-  const addAnimeToFavourite = anime => {
-    dispatch({type: 'ADD_ANIME_TO_FAVOURITE' , payload: anime})
+  const addAnimeToFavourite = async anime => {
+    const animeCopy = {...anime, uid: auth.currentUser.uid}
+    try {
+      const ref = collection(db, 'anime')
+      const addedAnime = await addDoc(ref, animeCopy)
+      dispatch({type: 'ADD_ANIME_TO_FAVOURITE' , payload: addedAnime})
+    } catch (error) {
+      console.log(error.message)
+    }
   }
 
-  const removeAnimeFromFavourite = (id) => {
-    dispatch({type: 'REMOVE_ANIME_FROM_FAVOURITE', payload: id})
+  const removeAnimeFromFavourite = async (id) => {
+    await deleteDoc(doc(db, 'anime', id))
+    dispatch({type: 'REMOVE_ANIME_FROM_FAVOURITE'})
   }
 
   return <FavouriteAnimeContext.Provider value={{
