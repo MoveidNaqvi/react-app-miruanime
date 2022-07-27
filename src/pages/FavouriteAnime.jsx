c2cda300605abf119073a59fb66b16a95e6968b0
MoveidNaqvi <moviednaqvi@gmail.com>
2022-07-27 12:17:57 +0100

    map only anime which match the uid of the user with the uid on the anime.

--- src/pages/FavouriteAnime.jsx
+++ src/pages/FavouriteAnime.jsx
@@ -1,25 +1,45 @@
-import { useContext } from 'react'
-import FavouriteAnimeContext from '../context/FavouriteAnimeContext'
+import { useEffect, useState } from 'react'
 import AnimeList from "../components/animelist/AnimeList"
 import './FavouriteAnime.css'
+import { collection, onSnapshot, query, where } from 'firebase/firestore'
 import { auth } from '../firebase/config'
+import { db } from '../firebase/config'
 
 function FavouriteAnime() {
 
-  const {favourites} = useContext(FavouriteAnimeContext)
-  const [favouriteAnimeArray] = [{data: favourites}]
+  const [documents, setDocuments] = useState(null)
+  const [loading, isLoading] = useState(true)
 
   const handleLogout = () => {
     auth.signOut()
   }
 
+  useEffect(() => {
+    let ref = collection(db, 'anime')
+    const q = query(ref, where('uid', '==', auth.currentUser.uid))
+    const unsub = onSnapshot(q, (snapshot) => {
+      let results = []
+      snapshot.docs.forEach(doc => {
+        results.push({...doc.data(), id: doc.id})
+      })
+      setDocuments(results)
+      isLoading(false)
+    })
+    return unsub
+  },[])
+
+  const [favouriteAnimeArray] = [{data: documents}]
+
   return (
-    <div className='fav-anime'>
-      <h1>Welcome {auth.currentUser.displayName}</h1>
-      {favouriteAnimeArray.data.length > 0 ? <h1>Your favourite anime!</h1> : ''}
-      {favouriteAnimeArray.data.length > 0 ? <AnimeList animelist={favouriteAnimeArray} type='favourite'/> : <h1>No favourite anime!</h1>}
-      <button onClick={handleLogout}>Logout</button>
-    </div>
+    <>
+      {!loading && 
+      <div className='fav-anime'>
+        <h1>Welcome {auth.currentUser.displayName}</h1>
+        {favouriteAnimeArray.data.length > 0 ? <h1>Your favourite anime!</h1> : ''}
+        {favouriteAnimeArray.data.length > 0 ? <AnimeList animelist={favouriteAnimeArray} type='favourite'/> : <h1>No favourite anime!</h1>}
+        <button onClick={handleLogout}>Logout</button>
+      </div>}
+    </>
   )
 }
 
