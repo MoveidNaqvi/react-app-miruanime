b515164aaaef018eaa229b62118c47ab02c58e0e
MoveidNaqvi <moviednaqvi@gmail.com>
2022-07-29 12:00:02 +0100

    Changed link to access about page

--- src/pages/FavouriteAnime.jsx
+++ src/pages/FavouriteAnime.jsx
@@ -1,18 +1,16 @@
 import { useEffect, useState } from 'react'
+import {Link} from 'react-router-dom'
 import AnimeList from "../components/animelist/AnimeList"
 import './FavouriteAnime.css'
 import { collection, onSnapshot, query, where } from 'firebase/firestore'
 import { auth } from '../firebase/config'
 import { db } from '../firebase/config'
+import { BsQuestionCircleFill } from 'react-icons/bs'
 
 function FavouriteAnime() {
 
   const [documents, setDocuments] = useState(null)
   const [loading, isLoading] = useState(true)
-
-  const handleLogout = () => {
-    auth.signOut()
-  }
 
   useEffect(() => {
     let ref = collection(db, 'anime')
@@ -37,7 +35,7 @@ function FavouriteAnime() {
         <h1>Welcome {auth.currentUser.displayName}</h1>
         {favouriteAnimeArray.data.length > 0 ? <h1>Your favourite anime!</h1> : ''}
         {favouriteAnimeArray.data.length > 0 ? <AnimeList animelist={favouriteAnimeArray} type='favourite'/> : <h1>No favourite anime!</h1>}
-        <button onClick={handleLogout}>Logout</button>
+        <span className='about-me-link'><Link to='/about'><BsQuestionCircleFill className='question' size={40}/></Link></span>
       </div>}
     </>
   )
