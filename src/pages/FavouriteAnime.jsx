600eb6e030d912378129b7ce0e2fa281e17aa538
MoveidNaqvi <moviednaqvi@gmail.com>
2022-08-05 16:50:11 +0100

    added useEffect to get anime stored in favourites array

--- src/pages/FavouriteAnime.jsx
+++ src/pages/FavouriteAnime.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
 import AnimeList from "../components/animelist/AnimeList"
 import './FavouriteAnime.css'
-import { collection, onSnapshot, query, where } from 'firebase/firestore'
+import { doc, onSnapshot} from 'firebase/firestore'
 import { auth } from '../firebase/config'
 import { db } from '../firebase/config'
 import { BsQuestionCircleFill } from 'react-icons/bs'
@@ -13,30 +13,32 @@ function FavouriteAnime() {
   const [loading, isLoading] = useState(true)
 
   useEffect(() => {
-    let ref = collection(db, 'anime')
-    const q = query(ref, where('uid', '==', auth.currentUser.uid))
-    const unsub = onSnapshot(q, (snapshot) => {
-      let results = []
-      snapshot.docs.forEach(doc => {
-        results.push({...doc.data(), id: doc.id})
-      })
-      setDocuments(results)
+    let ref = doc(db, 'users', auth.currentUser.uid)
+    // const q = query(ref, where('uid', '==', auth.currentUser.uid))
+    const unsub = onSnapshot(ref, (snapshot) => {
+      if(snapshot.data()){
+        setDocuments({...snapshot.data(), id: snapshot.id})
+        isLoading(false)
+      }else{
+        console.log('No document with that id')
+        isLoading(false)
+      }
+    }, (err) => {
+      console.log('Failed to get document')
       isLoading(false)
     })
     return unsub
   },[])
-
-  const [favouriteAnimeArray] = [{data: documents}]
+  const [favouriteAnimeArray] = [{data:  !loading && documents.favourites}]
 
   return (
     <>
-      {!loading && 
       <div className='fav-anime'>
         <h1>Welcome {auth.currentUser.displayName}</h1>
         {favouriteAnimeArray.data.length > 0 ? <h1>Your favourite anime!</h1> : ''}
         {favouriteAnimeArray.data.length > 0 ? <AnimeList animelist={favouriteAnimeArray} type='favourite'/> : <h1>No favourite anime!</h1>}
         <span className='about-me-link'><Link to='/about'><BsQuestionCircleFill className='question' size={40}/></Link></span>
-      </div>}
+      </div>
     </>
   )
 }
