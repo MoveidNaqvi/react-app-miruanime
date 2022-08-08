83c097148b1c9e354ec032b9d548ffbed9a82567
MoveidNaqvi <moviednaqvi@gmail.com>
2022-08-08 17:50:03 +0100

    logic to appen newly added anime to favourites array while keeping the old ones

--- src/context/FavouriteAnimeContext.jsx
+++ src/context/FavouriteAnimeContext.jsx
@@ -1,7 +1,8 @@
 import { createContext, useReducer} from "react";
 import { auth, db } from "../firebase/config";
-import {setDoc, deleteDoc, doc} from 'firebase/firestore'
+import {deleteDoc, doc, getDoc, updateDoc} from 'firebase/firestore'
 import favouriteAnimeReducer from "./FavouriteAnimeReducer";
+import { useState } from "react";
 
 const FavouriteAnimeContext = createContext()
 
@@ -12,14 +13,29 @@ export const FavouriteAnimeProvider = ({children}) => {
   }
 
   const [state, dispatch] = useReducer(favouriteAnimeReducer, initialState)
+  const [fav, setFav] = useState([])
+
+
+  const getFavRef = async () => {
+    const docRef = doc(db, 'users', auth.currentUser.uid)
+    const docSnap = await getDoc(docRef)
+    if(docSnap.exists()){
+      setFav(docSnap.data())
+    }
+    else{
+      console.log('error')
+    }
+  }
+
 
   const addAnimeToFavourite = async anime => {
+    getFavRef()
     const animeCopy = {...anime, uid: auth.currentUser.uid}
     try {
       const ref = doc(db, 'users', auth.currentUser.uid)
-      const addedAnime = await setDoc(ref, {
-        favourites: [animeCopy],
-        animeID: [anime.mal_id]
+      const addedAnime = await updateDoc(ref, {
+        favourites: [...fav.favourites, animeCopy],
+        animeID: [...fav.animeID, anime.mal_id]
       })
       dispatch({type: 'ADD_ANIME_TO_FAVOURITE' , payload: addedAnime})
     } catch (error) {
