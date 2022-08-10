1a24e170fb25fd4861bdffc52786043a69346907
MoveidNaqvi <moviednaqvi@gmail.com>
2022-08-10 11:42:26 +0100

    error fix that was not updating the favourites array after each deletion

--- src/components/favouriteAnimeControls/Controls.jsx
+++ src/components/favouriteAnimeControls/Controls.jsx
@@ -1,16 +1,24 @@
 import FavouriteAnimeContext from '../../context/FavouriteAnimeContext'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { TiTimes } from 'react-icons/ti'
 import './Controls.css'
 function Controls({type, anime}) {
 
-  const {removeAnimeFromFavourite} = useContext(FavouriteAnimeContext)
+  const {removeAnimeFromFavourite, getFavRef} = useContext(FavouriteAnimeContext)
+
+  const handleDelete = (mal_id) => {
+    removeAnimeFromFavourite(mal_id)
+  }
+
+  useEffect(() => {
+    getFavRef()
+  },[anime])
 
   return (
     <>
     {type === 'favourite' && (
       <div className="remove">
-        <button className="remove-btn" onClick={() => removeAnimeFromFavourite(anime.id)}><TiTimes className='x-icon'/></button>
+        <button className="remove-btn" onClick={() => handleDelete(anime.mal_id)}><TiTimes className='x-icon'/></button>
       </div>
     )}
     </>
