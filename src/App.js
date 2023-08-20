864459ebe59511b543e2a6ab33c08b493247ceac
Moveid Syed <moviednaqvi@gmail.com>
2023-08-20 12:24:32 +0100

    migrated to routes to latest version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {Route, createBrowserRouter, RouterProvider, createRoutesFromElements} from 'react-router-dom'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
-import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Error from './pages/Error';
 import Anime from './pages/Anime';
 import SearchPage from './pages/SearchPage';
-import Footer from './components/footer/Footer';
 import UpcomingAnime from './pages/UpcomingAnime';
 import TopAnime from './pages/TopAnime';
 import FavouriteAnime from './pages/FavouriteAnime';
@@ -15,30 +13,29 @@ import Signin from './pages/Signin/Signin'
 import { FavouriteAnimeProvider } from './context/FavouriteAnimeContext'
 import Signup from './pages/Signup/Signup';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import RootLayout from './pages/RootLayout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout/>}>
+      <Route index element={<Home />} />
+      <Route path='/upcoming' element={<UpcomingAnime />} />
+      <Route path='/top' element={<TopAnime />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/anime/:id' element={<Anime />} />
+      <Route path='/search' element={<SearchPage />} />
+      <Route path='/favourite' element={<PrivateRoute><FavouriteAnime /></PrivateRoute>} />
+      <Route path='/sign-in' element={<Signin />} />
+      <Route path='/sign-up' element={<Signup />} />
+      <Route path='/*' element={<Error />} />
+    </Route>
+  )
+)
 
 function App() {
   return (
     <FavouriteAnimeProvider>
-      <Router>
-        <div className='flex flex-col justify-between items-center min-h-screen h-full'>
-        <Navbar />
-        <div className=' max-w-[100rem]'>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/upcoming' element={<UpcomingAnime/>}/>
-            <Route path='/top' element={<TopAnime/>}/>
-            <Route path='/about' element={<About/>} />
-            <Route path='/anime/:id' element={<Anime/>}/>
-            <Route path='/search' element={<SearchPage/>} />
-            <Route path='/favourite' element={<PrivateRoute><FavouriteAnime/></PrivateRoute>}/>
-            <Route path='/sign-in' element={<Signin/>}/>
-            <Route path='/sign-up' element={<Signup/>}/>
-            <Route path='/*' element={<Error/>} />
-          </Routes>
-        </div>
-        <Footer/>
-        </div>
-      </Router>
+      <RouterProvider router={router}/>
       <ToastContainer toastStyle={{backgroundColor: '#222831', color: '#fff', fontFamily: 'inherit'}} closeButton={false}/>
     </FavouriteAnimeProvider>
   );
